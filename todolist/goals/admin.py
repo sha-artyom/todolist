cee9804e3c5768f00a15c6c17a303855ec484fd2
Vip-Artyom <snowboard-team@mail.ru>
2023-04-20 18:35:21 +0600

    Lesson_36(Goals)

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from todolist.goals.models import GoalCategory
+from todolist.goals.models import Goal, GoalCategory, GoalComment
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
-    search_fields = ('title',)
+    search_fields = ('title', 'user')
     list_filter = ('is_deleted',)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated', 'category', 'description', 'status', 'priority', 'due_date')
+    search_fields = ('title', 'description', 'user')
+    list_filter = ('status', 'priority')
+
+
+@admin.register(GoalComment)
+class GoalCommentAdmin(admin.ModelAdmin):
+    list_display = ('goal', 'user', 'text', 'created', 'updated')
+    search_fields = ('goal', 'text', 'user')
--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -23,3 +23,45 @@ class GoalCategory(BaseModel):
 
     def __str__(self):
         return self.title
+
+
+class Goal(BaseModel):
+    class Status(models.IntegerChoices):
+        to_do = 1, 'К выполнению'
+        in_progress = 2, 'В процессе'
+        done = 3, 'Выполнено'
+        archived = 4, 'Архив'
+
+    class Priority(models.IntegerChoices):
+        low = 1, 'Низкий'
+        medium = 2, 'Средний'
+        hight = 3, 'Высокий'
+        critical = 4, 'Критичный'
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(GoalCategory, on_delete=models.PROTECT, related_name='goals')
+    due_date = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='goals')
+    status = models.PositiveSmallIntegerField(choices=Status.choices, default=Status.to_do)
+    priority = models.PositiveSmallIntegerField(choices=Priority.choices, default=Priority.medium)
+
+    class Meta:
+        verbose_name = 'Цель'
+        verbose_name_plural = 'Цели'
+
+    def __str__(self):
+        return self.title
+
+
+class GoalComment(BaseModel):
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    text = models.TextField(verbose_name='Комментарий', max_length=255)
+    goal = models.ForeignKey(Goal, verbose_name='Цель', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.text
